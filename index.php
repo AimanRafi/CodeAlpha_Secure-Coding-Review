25a4f5d85b4e6194fe71d51bf2602bc093c61a9a
AimanRafi <126613126+AimanRafi@users.noreply.github.com>
2026-05-24 15:06:22 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Secure Coding Review System</title>
+</head>
+<body>
+
+    <h1> Secure Coding Review Project</h1>
+
+    <p>This is a secure login system project using PHP and MySQL.</p>
+
+    <a href="login.php">
+        <button>Go to Login Page</button>
+    </a>
+
+</body>
+</html>
